f011b2535626d9ba396300ba8b4319b76e594ca2
toichubekKyzy <toichubekovabarchyn@gmail.com>
2023-12-08 23:16:13 +0600

    8.12

--- 31github/strelki.cs
+++ 31github/strelki.cs
@@ -14,29 +14,6 @@ namespace _31github
             List <> options = ;
             int indexVkladki = 0;
 
-            for (int BookNumber = 0; BookNumber < options.Count; BookNumber++)
-            {
-                if (BookNumber == indexVkladki)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.BackgroundColor = ConsoleColor.White;
-                }
-                Console.WriteLine(options[BookNumber].Title);
-
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.Black;
-            }
-
-            keyInfo = Console.ReadKey(true);
-
-            if (keyInfo.Key == ConsoleKey.UpArrow) { indexVkladki = (indexVkladki - 1 + options.Count()) % options.Count(); }
-            if (keyInfo.Key == ConsoleKey.DownArrow) { indexVkladki = (indexVkladki + 1) % options.Count(); }
-
-            if (keyInfo.Key == ConsoleKey.Enter)
-            {
-                options[indexVkladki].OpenNextOperation();
-
-            }
         }
     }
 }
