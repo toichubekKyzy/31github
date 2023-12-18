ddf22fba8e012a4a540406a618dbe50b0032bf00
Barchyn <79153926+toichubekKyzy@users.noreply.github.com>
2023-12-18 20:37:46 +0600

    Update Note.cs

--- 31github/Note.cs
+++ 31github/Note.cs
@@ -1,11 +1,1 @@
-public class Note
-{
-    public string Title { get; set; }
-    public string Content { get; set; }
 
-    public Note(string title, string content)
-    {
-        Title = title;
-        Content = content;
-    }
-}
