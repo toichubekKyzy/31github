98570c5397f1a9fa3c443b464776a8176a2d8e6f
Barchyn <79153926+toichubekKyzy@users.noreply.github.com>
2023-12-19 22:47:55 +0600

    Update StaticClass.cs
    Del

--- 31github/StaticClass.cs
+++ 31github/StaticClass.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _31github
-{
-    internal class StaticClass
-    {
-    }
-}
