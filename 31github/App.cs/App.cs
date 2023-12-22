e794fe7fc7f58af38de7ab23559b238cbc08a0b0
toichubekKyzy <toichubekovabarchyn@gmail.com>
2023-12-22 23:33:56 +0600

    HomeWork 22.12.23

---
+++ 31github/App.cs/App.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public  class App
+{
+
+        private string systemPassword;
+        public App(string Password)
+        {
+            systemPassword = Password;
+        }
+        public void Startup ()
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш пароль: ");
+            string userPassword = Console.ReadLine();
+
+            User  newUser = (User)CreateUser(userName, userPassword);
+            HelloUser(newUser);
+        }
+         
+
+        private User? CreateUser(string username, string password )
+        {
+            if (password == systemPassword) 
+            {
+               return new User(username, password);
+            }
+            return null;
+        }
+
+        private void HelloUser(User? user)
+        { 
+            if (user != null) 
+            {
+                Console.WriteLine($"Привет, {user}!");
+            }
+            else
+            {
+                Console.WriteLine("Вам не удалось войти в систему!");
+            }
+
+        }
+
+ }
---
+++ 31github/User.cs/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct User
+{
+    public string Username { get; }
+    public string Password { get; }
+
+    public User (string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
+
